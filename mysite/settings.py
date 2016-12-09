7cfed8c21ec039675ab158d5063f1f490cc2dcae
jess010 <jfreasier@gmail.com>
2016-12-08 21:51:33 -0600

    Take two on that last commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 
 import os
+import my_secret_key
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'cqb9i*t95lec94iq=tq&epn@6u&nbo2zgm1v50%sop4kp^@!9#'
+SECRET_KEY = my_secret_key.key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -8,8 +8,13 @@ https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
+from django.contrib.staticfiles.handlers import StaticFilesHandler
+application = StaticFilesHandler(get_wsgi_application())
+
+path = '/home/jess010/django-girls-project'
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
 
